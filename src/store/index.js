cc6a9cd48c818d5d80b81d9dbf88729d7435999a
MasterDoramu <qwerdc1234567890@gmail.com>
2021-06-21 14:42:00 +0300

    add state

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    tasks: []
+    tasks: [
+      {id: 1, title: 'сходить в магазин', date: new Date("2021-06-19"), comleted: false},
+      {id: 2, title: 'сходить на собеседование', date: new Date("2021-06-21"), comleted: false},
+      {id: 3, title: 'сходить за водой', date: new Date("2021-06-20"), comleted: false},
+      {id: 4, title: 'заниматься программировнием', date: new Date(), comleted: false},
+    ]
   },
   mutations: {
   },
